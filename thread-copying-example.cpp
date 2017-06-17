b8e8641de2a08f8a3d04d21ac98c5af52194190c
Oleg Tsarkov <olegts@olegts.haze.yandex.net>
2017-06-17 14:20:36 +0300

    Adding bad refference passing example.

---
+++ thread-copying-example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+
+class Modifyable {
+public:
+    Modifyable()
+    : a(0)
+    {}
+    void increment() {
+        ++a;
+    }
+    void output() {
+        std::cout << "a = " << a << "\n";
+    }
+private:
+    int a;
+};
+
+void modify(Modifyable& modifyable) {
+    modifyable.increment();
+    std::cout << "Just after modifying: ";
+    modifyable.output();
+}
+
+int main() {
+    Modifyable modifyable;
+    modify(modifyable);
+    modifyable.output();
+    std::thread t(modify, modifyable);
+    t.join();
+    modifyable.output();
+    return 0;
+}
