e727ac62288cd34c29af355f5ac568f2b52335ec
Oleg Tsarkov <olegts@olegts.haze.yandex.net>
2017-06-20 22:24:11 +0300

    Remembering all hierarchical levels in set.

--- hierarchical-mutex.cpp
+++ hierarchical-mutex.cpp
@@ -4,59 +4,56 @@
 #include <climits>
 #include <stdexcept>
 #include <cstdio>
+#include <set>
 
 class HeirarchicalMutex {
 public:
     HeirarchicalMutex(const size_t level)
-    : prev_hierarchical_level(0)
-    , this_mutex_hierarchical_level(level)
+    : this_mutex_hierarchical_level(level)
     {}
 
     void lock() {
         check_level();
         mu.lock();
-        update_hierarchical_level();
+        active_levels.insert(this_mutex_hierarchical_level);
     }
     void unlock() {
-        current_thread_hierarchical_level = prev_hierarchical_level;
+        active_levels.erase(this_mutex_hierarchical_level);
         mu.unlock();
     }
     bool  try_lock() {
         check_level();
         bool is_locked = mu.try_lock();
         if (is_locked) {
-            update_hierarchical_level();
+            active_levels.insert(this_mutex_hierarchical_level);
         }
         return is_locked;
     }
 private:
     void check_level() {
-        if (current_thread_hierarchical_level < this_mutex_hierarchical_level) {
+        if (active_levels.size() > 0 && *active_levels.begin() < this_mutex_hierarchical_level) {
             std::string str(50, ' ');
             sprintf(&str[0], "Hierarchy violation: %lu > %lu",
-                        this_mutex_hierarchical_level, current_thread_hierarchical_level);
+                        this_mutex_hierarchical_level, *active_levels.begin());
             throw std::logic_error(str);
         }
     }
-    void update_hierarchical_level() {
-        prev_hierarchical_level = current_thread_hierarchical_level;
-        current_thread_hierarchical_level = this_mutex_hierarchical_level;
-    }
-
     std::mutex mu;
-    size_t prev_hierarchical_level;
     size_t this_mutex_hierarchical_level;
-    static thread_local size_t current_thread_hierarchical_level;
+    static thread_local std::set<size_t> active_levels;
 };
 
-thread_local size_t HeirarchicalMutex::current_thread_hierarchical_level = ULLONG_MAX;
+thread_local std::set<size_t> HeirarchicalMutex::active_levels;
 
 int main() {
     HeirarchicalMutex mu1(1);
-    HeirarchicalMutex mu2(10);
+    HeirarchicalMutex mu2(2);
+    HeirarchicalMutex mu3(3);
     std::thread t1([&] {
-         std::lock_guard<HeirarchicalMutex> lock(mu1);
-         std::lock_guard<HeirarchicalMutex> lock2(mu2);
+        mu3.lock();
+        mu1.lock();
+        mu3.unlock();
+        mu2.lock();
     });
     t1.join();
     return 0;
