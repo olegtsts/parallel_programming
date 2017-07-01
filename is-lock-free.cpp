01d89c231bcfe930828154bf69a0b56a493fa442
Oleg Tsarkov <olegtsts@gmail.com>
2017-07-01 19:23:45 +0300

    Adding more checks

--- is-lock-free.cpp
+++ is-lock-free.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <memory>
 #include <atomic>
+#include <thread>
+
+
+struct __attribute__ ((packed))  Small {
+    int * b;
+    int a;
+};
 
 int main() {
+    std::atomic<std::thread::id> b;
+    std::atomic<void*> c;
     std::shared_ptr<int> a;
-    std::cout << std::boolalpha << std::atomic_is_lock_free(&a) << "\n";
+    std::atomic<Small> small;
+    std::cout << std::boolalpha << std::atomic_is_lock_free(&a) << " " << small.is_lock_free() << " "
+        << std::atomic_is_lock_free(&b) << " " << std::atomic_is_lock_free(&c) << " " << sizeof(int*) << "\n";
     return 0;
 }
