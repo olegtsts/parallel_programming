5aad385bdd32694aab4925cedfb285d0f9fedd60
Oleg Tsarkov <olegtsts@gmail.com>
2017-07-01 17:06:16 +0300

    Adding bool respresentation

--- is-lock-free.cpp
+++ is-lock-free.cpp
@@ -4,6 +4,6 @@
 
 int main() {
     std::shared_ptr<int> a;
-    std::cout << (std::atomic_is_lock_free(&a) ? 1 : 0) << "\n";
+    std::cout << std::boolalpha << std::atomic_is_lock_free(&a) << "\n";
     return 0;
 }
