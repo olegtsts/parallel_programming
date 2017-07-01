e3f38ba6ec8011bcbdce3c98481139d61b6d0d22
Oleg Tsarkov <olegtsts@gmail.com>
2017-07-01 23:52:40 +0300

    Adding more relaxed ordering.

--- lock-free-stack-with-ref-counting.cpp
+++ lock-free-stack-with-ref-counting.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <atomic>
 #include <memory>
+#include <thread>
+#include <sstream>
 
 template <typename T>
 class LockFreeStack {
@@ -12,26 +14,28 @@ public:
         CountedNodePtr new_node;
         new_node.ptr = new Node(data);
         new_node.external_count = 1;
-        new_node.ptr->next = head.load();
-        while (!head.compare_exchange_weak(new_node.ptr->next, new_node)) {}
+        new_node.ptr->next = head.load(std::memory_order_relaxed);
+        while (!head.compare_exchange_weak(new_node.ptr->next, new_node,
+                    std::memory_order_release, std::memory_order_relaxed)) {}
     }
     std::shared_ptr<T> pop() {
-        CountedNodePtr old_head = head.load();
+        CountedNodePtr old_head = head.load(std::memory_order_relaxed);
         while (true) {
             increase_head_count(old_head);
             Node* ptr = old_head.ptr;
             if (!ptr) {
                 return std::shared_ptr<T>();
             }
-            if (head.compare_exchange_strong(old_head, ptr->next)) {
+            if (head.compare_exchange_strong(old_head, ptr->next, std::memory_order_relaxed, std::memory_order_relaxed)) {
                 std::shared_ptr<T> res;
                 res.swap(ptr->data);
                 int count_increase = old_head.external_count - 2;
-                if (ptr->internal_count.fetch_add(count_increase) == -count_increase) {
+                if (ptr->internal_count.fetch_add(count_increase, std::memory_order_release) == -count_increase) {
                     delete ptr;
                 }
                 return res;
-            } else if (ptr->internal_count.fetch_sub(1) == 1) {
+            } else if (ptr->internal_count.fetch_sub(1, std::memory_order_relaxed) == 1) {
+                ptr->internal_count.load(std::memory_order_acquire);
                 delete ptr;
             }
         }
@@ -42,8 +46,8 @@ public:
 private:
     struct Node;
     struct CountedNodePtr {
-        int external_count;
-        Node* ptr;
+        int external_count = 0;
+        Node* ptr = nullptr;
     };
     struct Node {
         std::shared_ptr<T> data;
@@ -60,14 +64,40 @@ private:
         do {
             new_counter = old_counter;
             ++new_counter.external_count;
-        } while (!head.compare_exchange_strong(old_counter, new_counter));
+        } while (!head.compare_exchange_strong(old_counter, new_counter, std::memory_order_acquire, std::memory_order_relaxed));
         old_counter.external_count = new_counter.external_count;
     }
     std::atomic<CountedNodePtr> head;
 };
 int main() {
     LockFreeStack<int> stack;
-    stack.push(5);
-    std::cout << *stack.pop() << std::endl;
+    std::thread t1([&stack] {stack.push(1);});
+    std::thread t2([&stack] {stack.push(2);});
+    std::thread t3([&stack] {stack.push(3);});
+
+    std::thread t4([&stack] {
+        std::stringstream ss;
+        auto p = stack.pop().get();
+        ss << (p ? *p : 0) << std::endl;
+        std::cout << ss.str();
+    });
+    std::thread t5([&stack] {
+        std::stringstream ss;
+        auto p = stack.pop().get();
+        ss << (p ? *p : 0) << std::endl;
+        std::cout << ss.str();
+    });
+    std::thread t6([&stack] {
+        std::stringstream ss;
+        auto p = stack.pop().get();
+        ss << (p ? *p : 0) << std::endl;
+        std::cout << ss.str();
+    });
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
     return 0;
 }
