b476373b48e34b74d383452f3b5248c78d94ee21
uweb <olegtsts@gmail.com>
2017-07-20 21:45:12 +0300

    Seperating EdgeHandler logic

--- numerator.cpp
+++ numerator.cpp
@@ -11,6 +11,9 @@ class Edge{};
 
 class MessageProcessorBase {
 public:
+    template <typename Sender>
+    void Ping(const Sender&) {}
+
     virtual ~MessageProcessorBase(){}
 };
 
@@ -23,143 +26,154 @@ template <typename ... Args>
 class Piper {
 protected:
     template <typename GlobalPiper>
-    class ReceiveHandler {
+    class EdgeProxy {
     public:
-        virtual void CallReceive(GlobalPiper * piper) = 0;
-        virtual ~ReceiveHandler() {}
+        EdgeProxy(GlobalPiper& piper)
+        : piper(piper)
+        {}
+
+        virtual void NotifyAboutMessage() const = 0;
+        virtual ~EdgeProxy() {}
+    protected:
+        GlobalPiper& piper;
     };
-    int max_to_index;
+    int max_message_processor_index;
     int max_edge_index;
     std::vector<std::vector<int>> dest_pipes;
     std::vector<std::deque<std::unique_ptr<MessageBase>>> queues;
     std::vector<std::unique_ptr<MessageProcessorBase>> message_processors;
 public:
     Piper()
-    : max_to_index(0)
+    : max_message_processor_index(0)
     , max_edge_index(0)
     {
     }
 
-    template <typename To>
-    int GetToIndexImpl(const TypeSpecifier<To>&) {
+    template <typename MP>
+    int GetMessageProcessorIndexImpl(const TypeSpecifier<MP>&) {
         return -1;
     }
 
     template <typename GlobalPiper>
-    void FillReceiveHandlersImpl(std::vector<std::unique_ptr<Piper::ReceiveHandler<GlobalPiper>>>&) {
-    }
-    void GetEdgeIndexImpl() {}
+    void FillEdgeProxysImpl(std::vector<std::unique_ptr<Piper::EdgeProxy<GlobalPiper>>>&, GlobalPiper&) {}
     void GetAssosiatedEdgesImpl() {}
-    void PushMessageToQueueImpl() {}
+    void GetEdgeIndexImpl() {}
 
     virtual ~Piper() {}
 };
 
+template <typename T>
+class ReceivingFrom {};
+
 template <typename From, typename To, typename Message, typename ... Args>
 class Piper<Edge<From, To, Message>, Args...> : public Piper<Args...> {
-private:
-    int cur_to_index;
-    int cur_edge_index;
-    std::vector<std::unique_ptr<Piper<>::ReceiveHandler<Piper>>> receive_handlers;
-
-    template <typename From2>
-    class SenderProxy {
-    private:
-        Piper& piper;
-    public:
-        SenderProxy(Piper& piper)
-        : piper(piper)
-        {}
-
-        template <typename To2, typename Message2>
-        void Send(std::unique_ptr<Message2> message) const {
-            piper.PushMessageToQueueImpl(TypeSpecifier<From2>(), TypeSpecifier<To2>(), std::move(message));
-        }
-    };
 protected:
     template <typename GlobalPiper>
-    class ReceiveHandler : public Piper<Args...>::template ReceiveHandler<GlobalPiper> {
-    public:
-        using Piper<Args...>::template ReceiveHandler<GlobalPiper>::ReceiveHandler;
+    class EdgeProxy : public Piper<Args...>::template EdgeProxy<GlobalPiper> {
+    private:
+        template <typename From2>
+        class SenderProxy {
+        public:
+            SenderProxy(GlobalPiper& piper)
+            : piper(piper)
+            {}
 
-        virtual void CallReceive(GlobalPiper * piper) {
-            auto cur_parent = dynamic_cast<Piper *>(piper);
-            To* message_processor = dynamic_cast<To*>(cur_parent->message_processors[cur_parent->cur_to_index].get());
-            auto& current_queue = cur_parent->queues[cur_parent->cur_edge_index];
+            template <typename To2, typename Message2>
+            void Send(std::unique_ptr<Message2> message) const {
+                int edge_index = piper.GetEdgeIndexImpl(TypeSpecifier<Edge<From2, To2, Message2>>());
+                piper.queues[edge_index].push_back(std::move(message));
+            }
+        private:
+            GlobalPiper& piper;
+        };
+    public:
+        using Piper<Args...>::template EdgeProxy<GlobalPiper>::EdgeProxy;
+        using Piper<Args...>::template EdgeProxy<GlobalPiper>::piper;
+
+        virtual void NotifyAboutMessage() const {
+            auto& cur_piper = *dynamic_cast<Piper *>(&piper);
+            To* message_processor = dynamic_cast<To*>(cur_piper.message_processors[cur_piper.cur_to_index].get());
+            auto& current_queue = cur_piper.queues[cur_piper.cur_edge_index];
             Message* value = dynamic_cast<Message*>(current_queue.front().get());
             current_queue.pop_front();
-            message_processor->Receive(TypeSpecifier<From>(), *value, piper->GetSenderProxy<To>());
+            message_processor->Receive(ReceivingFrom<From>(), *value, SenderProxy<To>(piper));
         }
-        virtual ~ReceiveHandler() {}
+        virtual ~EdgeProxy() {}
     };
 public:
-    using Piper<Args...>::max_to_index;
-    using Piper<Args...>::GetToIndexImpl;
-    using Piper<Args...>::GetEdgeIndexImpl;
+    using Piper<Args...>::max_message_processor_index;
+    using Piper<Args...>::GetMessageProcessorIndexImpl;
     using Piper<Args...>::max_edge_index;
     using Piper<Args...>::dest_pipes;
     using Piper<Args...>::GetAssosiatedEdgesImpl;
-    using Piper<Args...>::PushMessageToQueueImpl;
     using Piper<Args...>::queues;
     using Piper<Args...>::message_processors;
+    using Piper<Args...>::GetEdgeIndexImpl;
     Piper()
     : Piper<Args...>()
     {
-        cur_to_index = Piper<Args...>::GetToIndexImpl(TypeSpecifier<To>());
-        if (cur_to_index == -1) {
-            cur_to_index = max_to_index++;
-            dest_pipes.push_back(std::vector<int>());
-            message_processors.push_back(std::make_unique<To>());
-        }
+        AddMessageProcessorIfNotExists<From>(cur_from_index);
+        AddMessageProcessorIfNotExists<To>(cur_to_index);
         cur_edge_index = max_edge_index++;
         dest_pipes[cur_to_index].push_back(cur_edge_index);
     }
-    template <typename GlobalPiper>
-    void FillReceiveHandlersImpl(std::vector<std::unique_ptr<Piper<>::ReceiveHandler<GlobalPiper>>>& handlers) {
-        Piper<Args...>::template FillReceiveHandlersImpl<GlobalPiper>(handlers);
-        handlers.push_back(std::make_unique<ReceiveHandler<GlobalPiper>>());
+
+    template <typename MP>
+    void AddMessageProcessorIfNotExists(int& target_index) {
+        target_index = Piper<Args...>::GetMessageProcessorIndexImpl(TypeSpecifier<MP>());
+        if (target_index == -1) {
+            target_index = max_message_processor_index++;
+            dest_pipes.push_back(std::vector<int>());
+            message_processors.push_back(std::make_unique<MP>());
+        }
     }
+
+    template <typename GlobalPiper>
+    void FillEdgeProxysImpl(std::vector<std::unique_ptr<Piper<>::EdgeProxy<GlobalPiper>>>& handlers, GlobalPiper& piper) {
+        Piper<Args...>::template FillEdgeProxysImpl<GlobalPiper>(handlers, piper);
+        handlers.push_back(std::make_unique<EdgeProxy<GlobalPiper>>(piper));
+    }
+
     void LastLevelInit() {
-        FillReceiveHandlersImpl<Piper>(receive_handlers);
+        FillEdgeProxysImpl<Piper>(edge_handers, *this);
         queues = std::vector<std::deque<std::unique_ptr<MessageBase>>>(max_edge_index);
     }
 
-    int GetToIndexImpl(const TypeSpecifier<To>&) {
+    int GetMessageProcessorIndexImpl(const TypeSpecifier<From>&) {
+        return cur_from_index;
+    }
+    int GetMessageProcessorIndexImpl(const TypeSpecifier<To>&) {
         return cur_to_index;
     }
-    int GetEdgeIndexImpl(const TypeSpecifier<Edge<From,To,Message>>&) {
+    int GetEdgeIndexImpl(const TypeSpecifier<Edge<From, To, Message>>&) {
         return cur_edge_index;
     }
+
     std::vector<int> GetAssosiatedEdgesImpl(const TypeSpecifier<To>& specifier) {
-        return dest_pipes[GetToIndexImpl(specifier)];
-    }
-    void PushMessageToQueueImpl(const TypeSpecifier<From>&, const TypeSpecifier<To>&, std::unique_ptr<Message> message) {
-        queues[cur_edge_index].push_back(std::move(message));
+        return dest_pipes[GetMessageProcessorIndexImpl(specifier)];
     }
 
-    void CallReceive(const int index) {
-        receive_handlers[index].get()->CallReceive(this);
+    template <typename From2, typename To2, typename Message2>
+    void PushMessageToQueue(std::unique_ptr<Message2> message) {
+        int edge_index = GetEdgeIndexImpl(TypeSpecifier<Edge<From2, To2, Message2>>());
+        queues[edge_index].push_back(std::move(message));
     }
 
-    template <typename From2>
-    SenderProxy<From2> GetSenderProxy() {
-        return SenderProxy<From2>(*this);
-    }
-
-    template <typename From2,typename To2, typename Message2>
-    void SendFromTo(std::unique_ptr<Message2> message) {
-        auto sender = GetSenderProxy<From2>();
-        sender.template Send<To2>(std::move(message));
+    const Piper<>::EdgeProxy<Piper>* GetEdgeProxy(const int index) {
+        return edge_handers[index].get();
     }
 
     virtual ~Piper() {}
+private:
+    int cur_to_index;
+    int cur_from_index;
+    int cur_edge_index;
+    std::vector<std::unique_ptr<Piper<>::EdgeProxy<Piper>>> edge_handers;
 };
 
 template <typename ... Args>
 class MessagePassingTree : public Piper<Args...> {
 public:
-    using Piper<Args...>::SendFromTo;
-
     MessagePassingTree()
     : Piper<Args...>()
     {
@@ -190,8 +204,9 @@ class MessageProcessorA : public MessageProcessorBase {
 public:
     using MessageProcessorBase::MessageProcessorBase;
 
+
     template <typename Sender>
-    void Receive(const TypeSpecifier<MessageProcessorB>&, const DoubleMessage& value, const Sender& sender) {
+    void Receive(const ReceivingFrom<MessageProcessorB>&, const DoubleMessage& value, const Sender& sender) {
         std::cout << "MessageProcessorA: I have got int value from MessageProcessorB " << value.a << std::endl;
         sender.template Send<MessageProcessorB>(std::make_unique<IntMessage>(2 + static_cast<int>(value.a)));
     }
@@ -203,12 +218,12 @@ public:
     using MessageProcessorBase::MessageProcessorBase;
 
     template <typename Sender>
-    void Receive(const TypeSpecifier<MessageProcessorA>&, const IntMessage& value, const Sender& sender) {
+    void Receive(const ReceivingFrom<MessageProcessorA>&, const IntMessage& value, const Sender& sender) {
         std::cout << "MessageProcessorB: I have got int value from MessageProcessorA " << value.a << std::endl;
         sender.template Send<MessageProcessorA>(std::make_unique<DoubleMessage>(3 + value.a));
     }
     template <typename Sender>
-    void Receive(const TypeSpecifier<MessageProcessorA>&, const DoubleMessage& value, const Sender&) {
+    void Receive(const ReceivingFrom<MessageProcessorA>&, const DoubleMessage& value, const Sender&) {
         std::cout << "MessageProcessorB: I have got double value from MessageProcessorA"  << value.a << std::endl;
     }
     virtual ~MessageProcessorB() {}
@@ -219,10 +234,10 @@ int main(){
         Edge<MessageProcessorA, MessageProcessorB, IntMessage>,
         Edge<MessageProcessorB, MessageProcessorA, DoubleMessage>> message_passing_tree;
 
-    message_passing_tree.SendFromTo<MessageProcessorB, MessageProcessorA>(std::make_unique<DoubleMessage>(1));
+    message_passing_tree.PushMessageToQueue<MessageProcessorB, MessageProcessorA>(std::make_unique<DoubleMessage>(1));
     for (size_t i = 0; i < 10; ++i) {
-        message_passing_tree.CallReceive(0);
-        message_passing_tree.CallReceive(1);
+        message_passing_tree.GetEdgeProxy(0)->NotifyAboutMessage();
+        message_passing_tree.GetEdgeProxy(1)->NotifyAboutMessage();
     }
     return 0;
 }
