66105cd482452278154da151e3bbf2083d6559b0
Oleg Tsarkov <olegtsts@gmail.com>
2017-08-05 18:08:20 -0400

    Exception-safe way to get map value

--- proto-test.cpp
+++ proto-test.cpp
@@ -11,6 +11,7 @@ int main() {
     person.mutable_example()->operator[](5) = 10;
     std::string str_person;
     person.SerializeToString(&str_person);
+    std::cout << "Size = " << str_person.size() << std::endl;
     tutorial::Person parsed_person;
     parsed_person.ParseFromString(str_person);
     std::cout << parsed_person.name() << std::endl;
@@ -18,14 +19,19 @@ int main() {
     std::cout << parsed_person.DebugString() << std::endl;
     std::cout << parsed_person.example().at(4) << std::endl;
 
+    std::cout << "Output from file\n";
     std::ifstream fh("proto_dump.txt");
     tutorial::Person read_person;
     read_person.ParseFromIstream(&fh);
     std::cout << read_person.name() << std::endl;
     std::cout << read_person.x() << std::endl;
     std::cout << read_person.DebugString() << std::endl;
-    std::cout << read_person.example().at(4) << std::endl;
-
+    auto it = read_person.example().find(4);
+    if (it != read_person.example().end()) {
+        std::cout << it->second << std::endl;
+    } else {
+        std::cout << "Cannot find element\n" << std::endl;
+    }
 }
 //protoc proto.proto --js_out=library=person,binary:. --cpp_out=.
 //g++-5 proto-test.cpp proto.pb.cc -lprotobuf
